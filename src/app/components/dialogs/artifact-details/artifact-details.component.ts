4f747a8c49ce36618e889d1680cd8fde2edeb627
Daniela Zorrilla <38742100+DanielaZorrilla@users.noreply.github.com>
2021-06-22 22:05:34 -0400

    Artifact details (vista de visitante)

--- src/app/components/dialogs/artifact-details/artifact-details.component.ts
+++ src/app/components/dialogs/artifact-details/artifact-details.component.ts
@@ -2,6 +2,10 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Artifact } from 'src/app/models/artifact.model';
 
+//--------------------------------------------------------------------------
+import { ApiService } from 'src/app/services/api.service';
+//--------------------------------------------------------------------------
+
 interface DialogData {
   artifact: Artifact;
 }
@@ -14,12 +18,23 @@ interface DialogData {
 export class ArtifactDetailsComponent implements OnInit {
 
   constructor(
+
+    //-----------------------------------------------------------------------
+    private api: ApiService,
+    //-----------------------------------------------------------------------
+
     public dialogRef: MatDialogRef<ArtifactDetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
   ) { }
 
   ngOnInit(): void {
   }
+
+
+  //-------------------------------------------------------------------
+  userRole = this.api.userRole;
+  //-------------------------------------------------------------------
+
 
   onNoClick(): void {
     this.dialogRef.close();
