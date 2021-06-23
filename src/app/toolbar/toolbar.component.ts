516eb894aea6de5222a521768660f9fe90b2f51b
Daniela Zorrilla <38742100+DanielaZorrilla@users.noreply.github.com>
2021-06-22 22:08:20 -0400

    Styling toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+//-----------------------------------------------------------------------
+import { ApiService } from 'src/app/services/api.service';
+//-----------------------------------------------------------------------
+
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  //constructor() { }
+
+  //------------------------------------------------------
+  constructor(
+    private api: ApiService,
+  ) { }
+  //------------------------------------------------------
 
   ngOnInit(): void {
   }
 
+  //------------------------------------------------------
+  userRole = this.api.userRole;
+  //------------------------------------------------------
 }
